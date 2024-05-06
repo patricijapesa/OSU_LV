24eecf67d7ba4f49253a5c2494393a239a8ba719
patricijapesa <163465574+patricijapesa@users.noreply.github.com>
2024-05-06 14:08:55 +0200

    LV4.py

--- LV4.py
+++ LV4.py
@@ -69,18 +69,16 @@ print('R2 score: ', R2)
 ohe = OneHotEncoder()
 X_encoded = ohe.fit_transform(data[['Fuel Type']]).toarray()
 
-numerical_features = data.select_dtypes(include='number')
+numerical_features = data.select_dtypes(include = 'number')
 ohe_columns = ohe.get_feature_names_out(['Fuel Type'])
-X_encoded_df = pd.DataFrame(X_encoded, columns=ohe_columns, index=data.index)
-numerical_features = pd.concat([numerical_features, X_encoded_df], axis=1)
+X_encoded_df = pd.DataFrame(X_encoded, columns = ohe_columns, index = data.index)
+numerical_features = pd.concat([numerical_features, X_encoded_df], axis = 1)
 
-X_train, X_test, y_train, y_test = train_test_split(numerical_features.drop(['CO2 Emissions (g/km)'], axis=1), numerical_features['CO2 Emissions (g/km)'], test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(numerical_features.drop(['CO2 Emissions (g/km)'], axis = 1), numerical_features['CO2 Emissions (g/km)'], test_size = 0.2, random_state = 1)
 
 linearModel = lm.LinearRegression()
 linearModel.fit(X_train, y_train)
 print('Model coefficients: ', linearModel.coef_)
-
-
 
 y_test_prediction = linearModel.predict(X_test)
 plt.scatter(y_test, y_test_prediction)
