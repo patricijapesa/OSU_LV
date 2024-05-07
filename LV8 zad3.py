8b102f1ae2cf6e4fdc116be673026e401a612776
patricijapesa <163465574+patricijapesa@users.noreply.github.com>
2024-05-07 15:10:37 +0200

    LV8 zad3.py

--- LV8 zad3.py
+++ LV8 zad3.py
@@ -10,7 +10,7 @@ model = load_model('model.keras')
 img = image.load_img('test1.png', target_size = (28, 28), color_mode = 'grayscale')
 img_array = image.img_to_array(img)
 
-img_array = img_array.astype('float32') / 255
+img_array = img_array.astype("float32") / 255
 img_array_s = np.expand_dims(img_array, -1)
 
 img_array_vector = img_array_s.reshape(-1, 784)
